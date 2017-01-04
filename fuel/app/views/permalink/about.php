176bdee458a7dfa07dee292baf11ed38f105fa3d
mtoksuy <mtoksuy@programmerbox.com>
2017-01-04 14:10:14 +0900

    version_1.0 about変更

--- fuel/app/views/permalink/about.php
+++ fuel/app/views/permalink/about.php
@@ -5,17 +5,21 @@
 		<p><img width="320" height="168" alt="" title="" src="http://sharetube.jp/assets/img/about/about_1.jpg" class="great_image_100"></p>
 -->
 
-<h2 class="heading_6">mtoksuy.Photoについて</h2>
+<h2 class="heading_6">このサイトについて</h2>
 
-<p>FUJIFILMのコンパクトデジタルカメラ「X70」のみでどれくらいまで自由に表現できるかを研究・公開するために作成されたマツオカソウヤのピクチャーライブラリーサイトです。</p>
+<p>FUJIFILMのコンパクトデジタルカメラ「X70」のみでどれくらいまで自由に表現できるかを研究・公開するために作成された松岡 鈴のピクチャーライブラリーサイトです。</p>
 
 
-<h2 class="heading_6">運営者について</h2>
-	<img width="162" height="162" class="flat_Frame" src="<?php echo HTTP ?>assets/img/permalink/about/my_icon_1.jpg" style="float: left; margin: 0 10px 0 0;" alt="マツオカソウヤ">
-	<p>東京在住、マツオカソウヤ。</p>
-	<p>17歳の時にインターネットに出会いました。Yahooのオセロゲームだったのですが、画面の向こう側に同じく誰かが座っていてチャットをしながらオセロをしているという現実は僕には輝いて見えました。それからインターネットにのめり込み、webサービスに触れ、自分自身でwebサービスを作りたいと思うようになり、僕が体感したインターネットを他の人にも体感してもらいたい。そんな想いを込めてwebサービスを作っている人です。</p>
+<h2 class="heading_6">松岡 鈴</h2>
+	<img width="162" height="162" class="flat_Frame_no" src="<?php echo HTTP ?>assets/img/permalink/about/my_icon_2.png" style="float: left; margin: 0 10px 0 0;" alt="マツオカソウヤ">
+	<p class="m_0">東京在住、各地へ旅をしながら写真を撮っています。主にヌード/トラベル/スナップ/夜景/鉄/ご飯etc.</p>
+<p>ポートレート&スナップ<p>
+
+	<p>女性の肌にアートを宿すために。
+</p>
 	<br>
-			
-	<p>Instagram : <a href="https://www.instagram.com/mtoksuy/" target="_blank">@mtoksuy</a></p>
+<!--			
+	<p>Instagram : <a href="https://www.instagram.com/suz_matsuoka/" target="_blank">@suz_matsuoka</a></p>
+-->
 	</div>
 </section>
