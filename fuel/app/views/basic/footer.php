7a969e0260c1bfc53bf27f0fcf57209e434844a5
mtoksuy <mtoksuy@programmerbox.com>
2017-01-04 14:11:08 +0900

    version_1.0 微変更

--- fuel/app/views/basic/footer.php
+++ fuel/app/views/basic/footer.php
@@ -9,17 +9,17 @@
 					</div>
 					<!-- footer_contents_box -->
 					<div class="footer_contents_box clearfix">
-						<h5>mtoksuy.Photoについて</h5>
+						<h5>サイトインフォ</h5>
 						<ul>
-							<li><a href="<?php echo HTTP; ?>about/">mtoksuy.Photoについて</a></li>
+							<li><a href="<?php echo HTTP; ?>permalink/about/">松岡 鈴について</a></li>
 						</ul>
 					</div> <!--  -->
 
 					<!--  -->
 					<div class="footer_contents_box clearfix">
-						<h5>mtoksuy.Photoをフォローする</h5>
+						<h5>松岡 鈴をフォローする</h5>
 						<ul>
-					  	<li><a href="https://www.instagram.com/mtoksuy/" target="_blank">Instagram</a></li>
+					  	<li><a href="https://www.instagram.com/suz_matsuoka/" target="_blank">Instagram</a></li>
 						</ul>
 					</div> <!--  -->
 
@@ -30,7 +30,7 @@
 					<div class="footer_bottom_contents">
 						<!-- コピーライト -->
 						<section id="copy">
-							<p class="m_0"><span>&copy; <?php echo date('Y'); ?> </span><a href="<?php echo Uri::base(); ?>">mtoksuy.Photo</a></p>
+							<p class="m_0"><span>&copy; <?php echo date('Y'); ?> </span><a href="<?php echo Uri::base(); ?>">suz</a></p>
 						</section>
 					</div>
 				</div>
