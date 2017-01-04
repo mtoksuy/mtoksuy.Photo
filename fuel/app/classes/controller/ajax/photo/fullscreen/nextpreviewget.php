5e8dc93f94a49ef1ba53b353e9fc4653cbae7357
mtoksuy <mtoksuy@programmerbox.com>
2017-01-05 02:16:17 +0900

    version_1.1 fullscreen_ajax実装

--- fuel/app/classes/controller/ajax/photo/fullscreen/nextpreviewget.php
+++ fuel/app/classes/controller/ajax/photo/fullscreen/nextpreviewget.php
@@ -25,6 +25,19 @@ class Controller_Ajax_Photo_Fullscreen_Nextpreviewget extends Controller {
   ["next_image_url"]=>
   string(60) "http://localhost/suz/assets/img/article/2016/original/27.jpg"
 */
+		// 記事データ取得
+		$article_res        = Model_Article_Basis::article_get('article', $_POST['url_number_data']);
+		// 記事のメタ生成
+		$meta_html          = Model_Article_Html::article_meta_html_create($article_data_array, 168);
+		// 記事のHTML生成
+		$article_data_array = Model_Article_Html::article_html_create($article_res);
+		// 年取得
+		$year = $article_data_array['article_year_time'];
+		// PhotoのHTML生成
+		$photo_html = Model_Article_Html::photo_html_create($article_data_array, $year, false);
+
+
+
 		header('Content-Type: text/javascript; charset=utf-8');
 		$json_data = array(
 					'preview'            => $previous_next_array['preview'],
@@ -33,6 +46,9 @@ class Controller_Ajax_Photo_Fullscreen_Nextpreviewget extends Controller {
 					'next_url_number'    => $previous_next_array['next_url_number'],
 					'preview_image_url'  => $previous_next_array['preview_image_url'],
 					'next_image_url'     => $previous_next_array['next_image_url'],
+					'article_data_array' => $article_data_array,
+					'meta_html'          => $meta_html,
+					'photo_html'         => $photo_html,
 		);
 		return json_encode($json_data);
 	}
