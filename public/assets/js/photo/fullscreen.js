52a1f38641c35a1200e2a9892ee32a525490308d
mtoksuy <mtoksuy@programmerbox.com>
2017-01-05 03:27:31 +0900

    version_1.1 fullscreen実装途中_1

--- public/assets/js/photo/fullscreen.js
+++ public/assets/js/photo/fullscreen.js
@@ -10,16 +10,10 @@ $(function() {
 
 			// ローディング画像表示
 			$('#fullscreen').append('<img class="fullscreen_ajax_loding" src="'+http+'assets/img/ajax/ajax_loader_5.gif">');
-			setTimeout(function() {
-				// ローディング削除
-				$('.fullscreen_ajax_loding').remove();
-				// 画像変更
-				$('.fullscreen_image').attr('src', image_url_data);
-			}, 1000);
 
-			/*
+			/*******
 			ajax開始
-			*/
+			*******/
 			$.ajax({
 				type: 'POST', 
 				url: http + 'ajax/photo/fullscreen/nextpreviewget/',
@@ -33,7 +27,21 @@ $(function() {
 				// 成功処理
 			  success: function(data) {
 					p(data);
-					// 次があった場合
+					/********************
+					// フルスクリーン関連
+					********************/
+					setTimeout(function() {
+						// ローディング削除
+						$('.fullscreen_ajax_loding').remove();
+						// フルスクリーン画像変更
+						$('.fullscreen_image').attr('src', image_url_data);
+						$('.fullscreen_image').attr('full-image-href-data', image_url_data);
+						$('.fullscreen_image').attr('title', data['article_data_array']['article_title']);
+						$('.fullscreen_image').attr('alt', data['article_data_array']['article_title']);
+					}, 1000);
+					/***********************
+					// 次のphotoがあった場合
+					***********************/
 					if(data['next']) {
 						// ライトアローがなかった場合作る
 						if(!($('.fullscreen_arrow_right').length)) {
@@ -59,8 +67,6 @@ $(function() {
 						$('.fullscreen_arrow_right a').attr('url-number-data', data['preview_url_number']);
 						// before-primary-id-data変更
 						$('.fullscreen_arrow_right a').attr('before-primary-id-data', url_number_data);
-
-
 	/*
 	 ["preview"]=>
 	  string(32) "http://localhost/suz/article/25/"
@@ -76,6 +82,9 @@ $(function() {
 	  string(60) "http://localhost/suz/assets/img/article/2016/original/27.jpg"
 	*/
 					}
+						/**********************
+						次のphotoがなかった場合
+						**********************/
 						else {
 							// レフトアロー非表示
 							$('.fullscreen_arrow_left').css( {
@@ -90,7 +99,10 @@ $(function() {
 							// before-primary-id-data変更
 							$('.fullscreen_arrow_right a').attr('before-primary-id-data', url_number_data);
 						}
-					// 表面の情報書き変え
+					/********************
+					// 内部情報の書き変え
+					********************/
+					// urlの置き換え
 					history.replaceState('', '', url_data);
 					/*************************************
 					// urlの置き換え 
@@ -100,28 +112,63 @@ $(function() {
 					// urlの追加
 					history.pushState('','','aiueo');
 					*************************************/
+					// title書き変え
+					$('title').html(data['article_data_array']['article_title']);
+
+					$('meta[name="twitter:url"]').attr('content', );
+					$('meta[name="twitter:title"]').attr('content', );
+					$('meta[name="twitter:description"]').attr('content', );
+					$('meta[name="twitter:image"]').attr('content', );
+
+					$('meta[property="og:url"]').attr('content', );
+					$('meta[property="og:title"]').attr('content', );
+					$('meta[property="og:image"]').attr('content', );
+					$('meta[property="og:description"]').attr('content', );
+					$('meta[property="og:"]').attr('content', );
+					$('meta[property="og:"]').attr('content', );
+
+					/*************
+					表側の書き変え
+					*************/
 					// 書き換えるone_third取得
 					one_third_image = image_url_data.replace(/original/,'one_third');
-p(one_third_image);
+					// one_third画像情報書き変え
 					$('.article_photo_image').attr('src', one_third_image);
-					$('.article_photo_image').attr('title', '');
-					$('.article_photo_image').attr('alt', '');
-					$('.article_photo_image').attr('full-image-href-data', '');
+					$('.article_photo_image').attr('title', data['article_data_array']['article_title']);
+					$('.article_photo_image').attr('alt', data['article_data_array']['article_title']);
+					$('.article_photo_image').attr('full-image-href-data', one_third_image);
+					// レフトアロー書き変え
+					$('.arrow_left a').attr('href', data['next']);
+					// ライトアロー書き変え
+					$('.arrow_right a').attr('href', data['preview']);
+					// 記事情報全書き換え
+					$('.main_contents').html(data['article_data_array']['article_html']);
 /*
-photo
-article_photo
-
 main_contents clearfix
 article_list
 */
+
 /*
-<img class="article_photo_image" title="ぶらぶらとぶらぶらさがり" alt="ぶらぶらとぶらぶらさがり" src="http://localhost/suz/assets/img/article/2016/one_third/22.jpg" full-image-href-data="http://localhost/suz/assets/img/article/2016/original/22.jpg" style="visibility: visible; height: 100%; width: auto;" width="auto" height="300">
+'preview'            => $previous_next_array['preview'],
+'next'               => $previous_next_array['next'],
+'preview_url_number' => $previous_next_array['preview_url_number'],
+'next_url_number'    => $previous_next_array['next_url_number'],
+'preview_image_url'  => $previous_next_array['preview_image_url'],
+'next_image_url'     => $previous_next_array['next_image_url'],
+'article_data_array' => $article_data_array,
+	article_html
+	article_link
+	article_primary_id
+	article_thumbnail_image
+	article_title
+	article_value
+	article_year_time
+	social_share_html
+	tag_array
+'meta_html'          => $meta_html,
+'photo_html'         => $photo_html,
+
 */
-
-
-
-
-
 			  },
 				// エラー処理
 			  error: function(data) {
